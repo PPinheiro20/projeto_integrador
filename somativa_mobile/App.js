8b1483ace7a04b2b26faac6f2fdd01abdf538c45
PPinheiro20 <pedro.h.senai@aluno.senai.br>
2026-09-03 08:09:44 -0300

    feat: ColecaoItem.js e ColecaoScreen.js adicionados em sua devidas pastas, conexão com o app.js realizada

--- somativa_mobile/App.js
+++ somativa_mobile/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ColecaoScreen from './src/screens/ColecaoScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <ColecaoScreen/>
+      <StatusBar style="auto"/>
+    </>
   );
 }
 
